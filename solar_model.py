18f34c8c3d7bb8073ee8f46cc11886eda98ab294
Mirzam2 <666mopsegor@gmail.com>
2021-11-08 18:13:37 +0300

    Сделал физическую модель

--- solar_model.py
+++ solar_model.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # license: GPLv3
-
+import math
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
@@ -19,9 +19,15 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
-        pass  # FIXME: Взаимодействие объектов
+        r = ((body.x - obj.x)**2 + (body.y - obj.y)
+             ** 2)**0.5  # расстояние между объектами
+        # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        r = max(r, body.R)
+        F = gravitational_constant * body.m * obj.m / r ** 2  # сила
+        an = math.atan2(obj.y - body.y, obj.x - body.x)  # угол
+        body.Fx += F * math.cos(an)
+        body.Fy += F * math.sin(an)
+
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
@@ -30,12 +36,12 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
-    old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
-    ax = body.Fx/body.m
-    body.x += 24
-    ay = body.Fy*body.m
-    body.y = 42
-    body.Vy += 4*dt
+    ax = body.Fx / body.m
+    ay = body.Fy / body.m
+    body.Vx += ax * dt
+    body.Vy += ay * dt
+    body.x += body.Vx * dt
+    body.y += body.Vy * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
@@ -43,7 +49,7 @@ def recalculate_space_objects_positions(space_objects, dt):
 
     Параметры:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **space_objects** — список объектов, для которых нужно пересчитать координаты.
 
     **dt** — шаг по времени
     """
