a6bf0eda431e1eed21e717ed44747715a53d1848
Mirzam2 <666mopsegor@gmail.com>
2021-11-16 10:39:26 +0300

    Comit

--- solar_main.py
+++ solar_main.py
@@ -186,14 +186,9 @@ def main():
         last_time = cur_time
         drawer.update(space_objects, box)
         if len(space_objects) != 0:
-            saving_data_to_an_list(data, space_objects[0], space_objects[1],int(model_time))
-        time.sleep(1.0 / 60)
-<<<<<<< HEAD
+            saving_data_to_an_list(data, space_objects[1], space_objects[0],int(model_time))
     pg.quit()
 
-=======
-    
->>>>>>> 0b829e4d4a1eec67314126cd526cb962f344b7be
     print('Modelling finished!')
     go_plot(data)
     
